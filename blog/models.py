6d5be7e2c406f5ca1e3149e5a1e00925d8539054
Shen <etkids@163.com>
2015-10-09 15:43:22 -0400

    fix license and model definition

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     pub_date = models.DateTimeField('pub_date', auto_now_add=True)
     update_time = models.DateTimeField('update time', auto_now=True)
-    tag = models.ManyToManyField('Tag', null=True, blank=True)
+    tag = models.ManyToManyField('Tag', blank=True)
     content_raw = models.TextField()
     content = models.TextField(default='', null=True, blank=True)
     click = models.IntegerField(default=0)

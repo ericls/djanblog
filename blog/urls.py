98771b9318c1b79085124aea21a8b8e73d4e02ec
Shen <etkids@163.com>
2015-11-28 03:10:35 -0500

    better url naming

--- blog/urls.py
+++ blog/urls.py
@@ -3,13 +3,17 @@ from blog.views import IndexView, TagView, PostView
 
 urlpatterns = patterns(
     'blog.views',
-    url(r'^$', IndexView.as_view(), {'page': 1}, name='index_1'),
+    url(r'^$', IndexView.as_view(), {'page': 1}, name='index'),
     url(r'^feed/$', 'feed', name='feed'),
     url(r'^archive/$', 'archive', name='archive'),
     url(r'^post/(?P<slug>.*?)/$', PostView.as_view(), name='post'),
     url(r'^page/(?P<page>\d+)/$', IndexView.as_view(), name='index'),
-    url(r'^tag/(?P<slug>.*?)/page/(?P<page>\d+)/$', TagView.as_view(), name='tag'),
-    url(r'^tag/(?P<slug>.*?)/$', TagView.as_view(), name='tag_1'),
+    url(
+        r'^tag/(?P<slug>.*?)/page/(?P<page>\d+)/$',
+        TagView.as_view(),
+        name='tag'
+    ),
+    url(r'^tag/(?P<slug>.*?)/$', TagView.as_view(), name='tag'),
     url(r'^new/$', 'editor', name='new_post'),
     url(r'^edit/(?P<slug>.*?)/$', 'editor', name='edit_post'),
 )

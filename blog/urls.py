caa0b65557ff863696bbdb33103d175857da0f8a
Shen <etkids@163.com>
2016-03-06 17:12:59 -0500

    Django 1.9

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, url
-from blog.views import IndexView, TagView, PostView
+from blog.views import IndexView, TagView, PostView, archive, editor, feed
 
-urlpatterns = patterns(
-    'blog.views',
+urlpatterns = [
     url(r'^$', IndexView.as_view(), {'page': 1}, name='index'),
-    url(r'^feed/$', 'feed', name='feed'),
-    url(r'^archive/$', 'archive', name='archive'),
+    url(r'^feed/$', feed, name='feed'),
+    url(r'^archive/$', archive, name='archive'),
     url(r'^post/(?P<slug>.*?)/$', PostView.as_view(), name='post'),
     url(r'^page/(?P<page>\d+)/$', IndexView.as_view(), name='index'),
     url(
@@ -14,6 +13,6 @@ urlpatterns = patterns(
         name='tag'
     ),
     url(r'^tag/(?P<slug>.*?)/$', TagView.as_view(), name='tag'),
-    url(r'^new/$', 'editor', name='new_post'),
-    url(r'^edit/(?P<slug>.*?)/$', 'editor', name='edit_post'),
-)
+    url(r'^new/$', editor, name='new_post'),
+    url(r'^edit/(?P<slug>.*?)/$', editor, name='edit_post'),
+]
--- djanblog/urls.py
+++ djanblog/urls.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'djanblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^panel-eric/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
-)
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
